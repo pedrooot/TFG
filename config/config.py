621346ba88316825c092eeec0550d77c303b8630
pedrooot <pedromarting3@gmail.com>
2024-04-21 17:19:08 +0200

    feat(banner): improve colors

--- config/config.py
+++ config/config.py
@@ -1,4 +1,5 @@
 import requests
+from colorama import Fore, Style
 
 glucose_bot_version = "v1.0.0"
 def check_current_version():
@@ -29,12 +30,12 @@ def print_help(message=None):
 
 
 def print_banner():
-    banner = rf""" 
-      ________.__                                  __________        __   
-     /  _____/|  |  __ __   ____  ____  ______ ____\______   \ _____/  |_ 
-    /   \  ___|  | |  |  \_/ ___\/  _ \/  ___// __ \|    |  _//  _ \   __\
-    \    \_\  \  |_|  |  /\  \__(  <_> )___ \\  ___/|    |   (  <_> )  |  
-     \______  /____/____/  \___  >____/____  >\___  >______  /\____/|__|  
-            \/                 \/          \/     \/       \/             
-    Version: {glucose_bot_version} """
+    banner = rf""" {Fore.YELLOW}
+  ________.__                                  __________        __   
+ /  _____/|  |  __ __   ____  ____  ______ ____\______   \ _____/  |_ 
+/   \  ___|  | |  |  \_/ ___\/  _ \/  ___// __ \|    |  _//  _ \   __\
+\    \_\  \  |_|  |  /\  \__(  <_> )___ \\  ___/|    |   (  <_> )  |  
+ \______  /____/____/  \___  >____/____  >\___  >______  /\____/|__|  
+        \/                 \/          \/     \/       \/           {Style.RESET_ALL}  
+{Fore.BLUE}Version: {glucose_bot_version}{Style.RESET_ALL} """
     print(banner)
