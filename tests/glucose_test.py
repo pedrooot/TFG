5df96b53996798e6b29b160c19d28ab0279f77e0
pedrooot <pedromarting3@gmail.com>
2024-07-21 12:28:34 +0200

    fix(tests): fix tests for current glucose

--- tests/glucose_test.py
+++ tests/glucose_test.py
@@ -1,31 +1,17 @@
-import pytest
-from unittest.mock import patch
-import requests
+from datetime import datetime
+from unittest.mock import Mock, patch
 from src.glucose import glucose_value
 
-@pytest.fixture
-def mocked_post_request():
-    with patch('requests.post') as mocked_post:
-        mocked_post.return_value.json.return_value = {'data': {'authTicket': {'token': 'test_token'}}}
-        yield mocked_post
-
-@pytest.fixture
-def mocked_get_request():
-    with patch('requests.get') as mocked_get:
-        mocked_get.return_value.json.return_value = {'data': [{'patientId': 'test_patient_id'}]}
-        yield mocked_get
-
 @patch('src.glucose.get_cgm_data')
-def test_glucose_value(mocked_get_cgm_data, mocked_post_request, mocked_get_request):
+def test_glucose_value(mocked_get_cgm_data):
 
-    mocked_get_cgm_data.return_value = {'data': {'graphData': [{'Value': 100}, {'Value': 90}, {'Time': 'test_time'}]}}
+    current_time = datetime.now()
+    mocked_get_cgm_data.return_value = {'data': {'graphData': [{'Value': 100}, {'Value': 90}, {'Time': current_time}]}}
     
+    database = Mock()
+    database.get_last_escaneo_usuario.return_value = {'ultimo_escaneo': current_time, 'valor_actual': 100, 'valor_previo': 90}
+    actual_glucose, previous_glucose, last_time = glucose_value(user_id='test_user_id', database=database)
 
-    actual_glucose, previous_glucose, last_time = glucose_value()
-    mocked_post_request.assert_called_once()
-    mocked_get_request.assert_called_once()
-    mocked_get_cgm_data.assert_called_once()
-
-    assert actual_glucose == 90
-    assert previous_glucose == 100
-    assert last_time == 'test_time'
+    assert actual_glucose == 100
+    assert previous_glucose == 90
+    assert last_time == current_time
